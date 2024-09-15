f71ceccab524530f1381c4f930d00b62f9a96061
reis-bits <eduardorprincepe007@gmail.com>
2024-09-15 16:28:54 -0300

    chore(code):Comentários novos adicionados para melhorar o entendimento do código.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
+
+// Função que realiza a compra do ingresso pelo tipo.
 function comprar() {
+    // Recupera o tipo do ingersso definido no HTML.
     let tipoIngresso = document.getElementById('tipo-ingresso').value;
+
+    // Recupera a quantidade de ingressos escrita no HTML
     let quantidadeIngresso = parseInt(document.getElementById('qtd').value);
 
+    // Define como comprar e alterar o valor pelo tipo do ingresso.
     if (tipoIngresso == 'pista') {
+        // Padrão repetitivo que chama a função comprarPorTipo e executa com dois parâmetros, um com a variável de quantidade e outro com a de tipo.
         comprarPorTipo(quantidadeIngresso, tipoIngresso);
     }
 
@@ -15,14 +22,19 @@ function comprar() {
     }
 }
 
+// Função comprarPorTipo definida com dois parâmetros, qtd e tipo, substituídas pela quantidade e tipo do ingresso. 
 function comprarPorTipo(qtd, tipo) {
+    // Variáveis com a quantidade dos ingressos e seus tipos.
     let qtdPista = parseInt(document.getElementById('qtd-pista').innerHTML);
     let qtdSuperior = parseInt(document.getElementById('qtd-superior').innerHTML);
     let qtdInferior = parseInt(document.getElementById('qtd-inferior').innerHTML);
 
+    // Bloco de código porco que eu deveria usar uma função auxiliar ou um switch pra ficar mais limpo, mas funciona, é isso que importa.
     if (tipo === 'pista' && qtd > qtdPista) {
+        // Padrão repetitivo que diz: se o tipo do ingresso for x, e a quantidade for maior que a que tem desse ingresso, diga que a quantidade  é insuficiente e não faça nada.
         alert('Quantidade insuficiente');
     } else if (tipo === 'pista' && qtd <= qtdPista) {
+        // Do contrário, tira a quantidade definida desse tipo de ingresso e ajuste o HTML.
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').innerHTML = qtdPista;
         alert('Compra realizada!');
